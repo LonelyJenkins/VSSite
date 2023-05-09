0deaff0a3d76929680ec6bc6676ccb059c6e7cb0
LonelyJenkins <124760225+LonelyJenkins@users.noreply.github.com>
2023-05-09 01:01:01 -0500

    Add project files.

---
+++ VSSite/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+var oldSize = document.getElementById('pink-text').style.fontSize;
+
+function newSize() {
+    document.getElementById('pink-text').style.fontSize = "30px";
+}
+
+function revertSize() {
+    document.getElementById('pink-text').style.fontSize = oldSize;
+}
